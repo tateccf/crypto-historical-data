72a0ad28a2203f6cc70158e0f8e40d0d5fed414c
tateccf <francisco.bajogarcia@security.ntt>
2024-11-07 12:31:40 +0100

    Improved unit tests

--- src/binance/binance.service.spec.ts
+++ src/binance/binance.service.spec.ts
@@ -21,15 +21,15 @@ describe('BinanceService', () => {
   });
 
   describe('fetchHistoricalData', () => {
-    it('should fetch historical data successfully', async () => {
-      const MOCKED_SYMBOL = 'BTCUSDT';
-      const MOCKED_START_TIME = 'start-time';
-      const MOCKED_END_TIME = 'end-time';
-      const MOCKED_RESPONSE_DATA = [
-        [1111, '100'],
-        [1112, '200'],
-      ];
+    const MOCKED_SYMBOL = 'BTCUSDT';
+    const MOCKED_START_TIME = 'start-time';
+    const MOCKED_END_TIME = 'end-time';
+    const MOCKED_RESPONSE_DATA = [
+      [1111, '100'],
+      [1112, '200'],
+    ];
 
+    it('should fetch historical data successfully', async () => {
       mockedAxios.get.mockResolvedValueOnce({ data: MOCKED_RESPONSE_DATA });
 
       const result = await service.fetchHistoricalData(
@@ -52,6 +52,32 @@ describe('BinanceService', () => {
       );
 
       expect(result).toEqual(MOCKED_RESPONSE_DATA);
+    });
+
+    it('should throw an error if the request fails', async () => {
+      const MOCKED_ERROR = new Error('Failed to fetch data');
+
+      mockedAxios.get.mockRejectedValueOnce(MOCKED_ERROR);
+
+      await expect(
+        service.fetchHistoricalData(
+          MOCKED_SYMBOL,
+          MOCKED_START_TIME,
+          MOCKED_END_TIME,
+        ),
+      ).rejects.toThrow('Failed to fetch data');
+
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        `${process.env.BINANCE_BASE_URL}/api/v3/klines`,
+        {
+          params: {
+            symbol: MOCKED_SYMBOL,
+            interval: '1d',
+            startTime: MOCKED_START_TIME,
+            endTime: MOCKED_END_TIME,
+          },
+        },
+      );
     });
   });
 });
