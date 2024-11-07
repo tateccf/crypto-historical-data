0545ee369798f2e780135d1226ad5a3d61953501
tateccf <francisco.bajogarcia@security.ntt>
2024-11-07 10:32:56 +0100

    Added Configuration Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BinanceService } from './binance/binance.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, BinanceService],
 })
