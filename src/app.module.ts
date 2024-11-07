fd8f11d704b8f77aa7192f68a0dd855a934642ec
tateccf <francisco.bajogarcia@security.ntt>
2024-11-07 10:30:39 +0100

    Created Binance Service skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BinanceService } from './binance/binance.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BinanceService],
 })
 export class AppModule {}
