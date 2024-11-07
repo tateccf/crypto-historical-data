4d5c71e26a0d6b26a32d3ffd6286660d0d467fc7
tateccf <francisco.bajogarcia@security.ntt>
2024-11-07 10:57:02 +0100

    Added fetchHistorialData method to BinanceService

--- src/binance/binance.module.ts
+++ src/binance/binance.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BinanceService } from './binance.service';
-import { BinanceController } from './binance.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  controllers: [BinanceController],
   providers: [BinanceService],
 })
 export class BinanceModule {}
--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBinanceDto } from './dto/create-binance.dto';
-import { UpdateBinanceDto } from './dto/update-binance.dto';
+
+import axios from 'axios';
 
 @Injectable()
 export class BinanceService {
-  private readonly K_LINES_PATH = 'klines';
+  private readonly K_LINES_PATH = '/klines';
+
+  constructor() {}
 
   async fetchHistoricalData(
     symbol: string,
     interval: string,
     startTime: string,
     endTime: string,
-  ) {}
+  ) {
+    const historialDataEndpoint = `${process.env.BINANCE_BASE_URL}${this.K_LINES_PATH}`;
+
+    const response = await axios.get(historialDataEndpoint, {
+      params: { symbol, interval, startTime, endTime },
+    });
+
+    return response.data;
+  }
 }
